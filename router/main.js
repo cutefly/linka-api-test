cc62f9351431dcd820f44d86aa3e4e2598fe90f2
chris <chris@kpcard.co.kr>
2020-04-22 21:24:59 +0900

    void 구현

--- router/main.js
+++ router/main.js
@@ -10,7 +10,7 @@ module.exports = function(app)
         res.render('index.html')
     });
 
-     app.get('/linka',function(req,res){
+    app.get('/linka',function(req,res){
         var token = '';
         var orderNo = new Date().getTime();;
 
@@ -24,7 +24,7 @@ module.exports = function(app)
             }
         })
         .then(function(response) {
-            console.log(response);
+            //console.log(response);
             token = response.data.data;
             console.log('token: %s',  JSON.stringify(token));
 
@@ -39,9 +39,52 @@ module.exports = function(app)
 
     });
 
-    app.post('/next',function(req,res){
-        console.log('next : ' + req.body);
-        res.render('next')
+    app.post('/void/:id',function(req,res){
+        var token = '';
+        var id = req.params.id;
+
+        axios({
+            url: 'https://dev-linka.alphapay.co.kr/v1/auth/dapp/token',
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'partner-id': 'KRLW2003201605',
+                'lcns-key': 'rtv79XnB7j9ZGmDDgELULMddMAkuyRTSUWDfyNCD'
+            }
+        })
+        .then(function(response) {
+            //console.log(response);
+            token = response.data.data;
+            console.log('token: %s',  JSON.stringify(token));
+
+            axios({
+                url: 'https://dev-linka.alphapay.co.kr/v1/dapp/web/pg/cancel',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                'x_access_token': token,
+                'partner_id': 'KRLW2003201605',
+                'partner_order_id': id
+            })
+            .then(function(res2) {
+                console.log('result: %s', JSON.stringify(res2.data));
+                if (  res2.data.code == '0000') {
+                    res.ok();
+                } else {
+                    res.send(res2.data);
+                }
+            })
+        })
+    });
+
+    app.get('/next/:id',function(req,res){
+        var id = req.params.id;
+
+        console.log('partner_order_id : ' + id);
+        res.render('next', {
+            'partner_order_id': id
+        })
      });
      
      app.post('/callback',function(req,res){
